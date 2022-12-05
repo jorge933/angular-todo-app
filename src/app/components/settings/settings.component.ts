6ec80629f7e891a6fb21c25256ff43e6f090cc88
Jorge <jorgevitorp9@gmail.com>
2022-12-05 13:21:12 -0300

    📦 refactor(settings.component): generating the settings form dynamically

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Settings } from 'app/models/settings.model';
 
 @Component({
@@ -9,16 +9,21 @@ import { Settings } from 'app/models/settings.model';
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
-  slideToggleControl = new FormControl(false);
+  settingsForm: FormGroup;
+  settingsObjKeys = Object.keys(this.settings) as Array<keyof Settings>;
+  optionsDescription = {
+    deleteCompletedTasks: 'Delete completed tasks on init application',
+  };
 
-  constructor(@Inject(MAT_DIALOG_DATA) public settings: Settings) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public settings: Settings,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    const slideToggleControl = this.slideToggleControl;
-    slideToggleControl.setValue(this.settings.deleteCompletedTasks);
-
-    slideToggleControl.valueChanges.subscribe((boolean) => {
-      this.settings.deleteCompletedTasks = !!boolean;
-    });
+    this.settingsForm = this.formBuilder.group(this.settings);
+    this.settingsForm.valueChanges.subscribe(
+      (values) => (this.settings = values)
+    );
   }
 }
